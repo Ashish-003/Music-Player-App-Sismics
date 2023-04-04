ffa21b0b60f92534557df270b9f8f25bf718a1e3
somadattareddy <dattareddy1911@gmail.com>
2023-04-04 21:57:29 +0530

    Feature 1 Phase 2

--- music-web/src/main/webapp/src/app/service/User.js
+++ music-web/src/main/webapp/src/app/service/User.js
@@ -3,32 +3,36 @@
 /**
  * User service.
  */
-angular.module('music').factory('User', function(Restangular) {
+angular.module('music').factory('User', function (Restangular) {
   var userInfo = null;
-  
+
   return {
     /**
      * Returns user info.
      * @param force If true, force reloading data
      */
-    userInfo: function(force) {
+    userInfo: function (force) {
       if (userInfo == null || force) {
         userInfo = Restangular.one('user').get();
       }
       return userInfo;
     },
-    
+
     /**
      * Login an user.
      */
-    login: function(user) {
+    login: function (user) {
       return Restangular.one('user').post('login', user);
     },
-    
+
+    signup: function (user) {
+      return Restangular.one('user').post('signup', user);
+    },
+
     /**
      * Logout the current user.
      */
-    logout: function() {
+    logout: function () {
       return Restangular.one('user').post('logout', {});
     }
   }
